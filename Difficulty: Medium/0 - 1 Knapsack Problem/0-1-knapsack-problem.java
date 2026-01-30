1281e7ab23900b244770bb6003c65af8882cbdd9
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2026-01-30 14:30:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,77 +1,64 @@
 class Solution {
+    
+    
+    
+    public int solve(int W, int val[], int wt[], int n, int[][] dp){
+        
+        
+        // best case 
+        
+        if( n ==0 || W ==0){
+        return    dp[n][W] =0;
+        }
+        
+        
+        if(dp[n][W]  != -1){
+            return dp[n][W];
+        }
+        
+        
+        
+        
+         if( wt[n-1] > W){
+            return  dp[n][W] =    solve(W, val, wt, n-1,dp );
+        }
+        else {
+            
+            
+         return    dp[n][W] =    Math.max( 
+                  solve(W, val, wt, n-1,dp),
+         
+               val[n-1] +   solve(W-wt[n-1], val, wt, n-1,dp));
+            
+            
+            
+            
+        }
+    }
+    
+    
+    
+    
     public int knapsack(int W, int val[], int wt[]) {
+      
+      int n=wt.length;
+      
+      int[][] dp = new int[n+1][W+1];
+      
+      
+      for(int i =0; i <=n; i++){
+          for(int j =0; j <=W; j++){
+              dp[i][j] = -1;
+          }
+      }
+      
+      
+      
+      
+      return  solve(W, val, wt, n, dp);
         
+       
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // code here
-        int n = wt.length;
-        
-        
-        // dp array
-         int[][] dp = new int[n+1][W+1]; 
-         
-         
-         for(int i =0; i <n+1; i++){
-             for(int j =0; j <W+1; j++){
-                 
-                 // if n ==0 or w ==0 return 0;   n -> i and W -> j
-                 
-                 if( i == 0 || j ==0){
-                     dp[i][j] =0;
-                 }
-                 
-                 
-                 /// if wt > W
-                 
-                else  if( wt[i-1] > j){
-                     dp[i][j] = dp[i-1][j];
-                 }
-                 else{
-                     
-                     
-                     dp[i][j] = Math.max(
-                                  
-                                  dp[i-1][j],
-                                  val[i-1] + dp[i-1][j - wt[i-1]]
-                         
-                         );
-                     
-                     
-                     
-                     
-                 }
-                 
-                 
-                 
-                 
-             }
-         }
-         
-         return dp[n][W];
-         
         
     }
 }
