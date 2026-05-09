07365dada4245d188d4fc2162cfa0bb6901177d0
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2026-05-09 13:48:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,38 +1,40 @@
 class Solution {
-   
+   public void bfs(ArrayList<ArrayList<Integer>> adj, boolean[] visited, int source,ArrayList<Integer> result  ){
+         
+         Queue<Integer> q = new LinkedList<>();
+         q.add(source);
+         visited[source] = true;
+         
+         while(!q.isEmpty()){
+             
+             int removed = q.poll();
+             result.add(removed);
+             
+             for(int neigh : adj.get(removed)){
+                 if(!visited[neigh]){
+                     visited[neigh] =true;
+                     q.add(neigh);
+                 }
+             }
+             
+         }
+     }
+     
+     
+     
+     
+     
+     
+     
+     
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
         // code here
         ArrayList<Integer> result = new ArrayList<>();
-        int n = adj.size();
-
+        int n  = adj.size();
         boolean[] visited = new boolean[n];
-
-        getBfsSolve(adj, visited, result, 0);
-
+        
+        bfs(adj, visited,  0, result);
         return result;
+        
     }
-
-    private void getBfsSolve(ArrayList<ArrayList<Integer>> adj, boolean[] visited, List<Integer> result, int source) {
-
-        Queue<Integer> q = new LinkedList<>();
-        q.add(source);
-        visited[source] = true;
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            result.add(node);
-
-
-            for (int neigh : adj.get(node)) {
-                if (!visited[neigh]) {
-                    visited[neigh] = true;
-                    q.add(neigh);
-                }
-            }
-
-
-        }
-    }
-    
-    
 }
